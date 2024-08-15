e0404bc9557b60a53122c144e15de964d92de52d
Cole Aydelottte <cole.aydelotte@gmail.com>
2024-08-15 04:33:15 -0400

    added a demo video.
    added a demo video.

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     let user_input = stdin();
     functionality.output_files(forward_dirs.clone(), false);
     loop {
-        println!("Enter one of the following commands: [exit, in, up]");
+        println!("Enter one of the following commands: [exit, in, up, ls, pwd, or cls]");
         let mut input = String::new();
         user_input.read_line(&mut input).unwrap();
         input = input.trim().to_string();
@@ -26,10 +26,12 @@ fn main() {
             let new_path: PathBuf = functionality.step_in(forward_dirs.clone());
             directory.set_pwd(new_path.clone());
             println!("Stepping In A Directory: {}", new_path.display());
-            functionality.output_files(directory.find_forward_directories_and_files(), false);
+            functionality.clear_terminal();
+            functionality.output_files(directory.find_forward_directories(), false);
             forward_dirs = directory.find_forward_directories();
         }
         else if input.trim() == "up" {
+            functionality.clear_terminal();
             println!("Stepping Up A Directory: {}", functionality.step_up());
             directory.set_pwd(PathBuf::from(functionality.get_pwd()));
             forward_dirs = directory.find_forward_directories();
