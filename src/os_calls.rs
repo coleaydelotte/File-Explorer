fba43034ffc64949ef2e8f811ec24b16d12a47fc
Cole Aydelottte <cole.aydelotte@gmail.com>
2024-08-21 02:14:36 -0400

    Added some simple documentation.
    Added some simple documentation.

--- src/os_calls.rs
+++ src/os_calls.rs
@@ -1,4 +1,9 @@
 use std::process::Command;
+
+/**
+ * This function will open a file in the default program for the file type.
+ * It will use the OS-specific command to open the file.
+ */
 pub fn open_file(file_path: &str) {
     // This function will open a file in the default program for the file type.
     // It will use the OS-specific command to open the file.
