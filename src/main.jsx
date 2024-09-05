e36aa150ca6ccf2af696e4e2178d450dfa4359bf
Cole Aydelotte <aydelottec@moravian.edu>
2024-09-05 14:31:17 -0400

    Updated main.jsx to fix lag.
    Updated main.jsx to fix lag.

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // for some reason when running with react strict mode, it slows the app down by a lot
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+
+  // so I'm just going to run it without strict mode
+  <App />
 );
