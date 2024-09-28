bb0b07541cacfcd9b0e5629bb2b8406398284d76
Cole Aydelotte <aydelottec@moravian.edu>
2024-09-28 15:41:49 -0400

    Removed Comment never going to be used.
    Removed Comment never going to be used.

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,5 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // for some reason when running with react strict mode, it slows the app down by a lot
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-
-  // so I'm just going to run it without strict mode
   <App />
 );
