8ceb363bb48b58cae8f54866e4efc687090ab985
Cole Aydelottte <cole.aydelotte@gmail.com>
2025-05-13 02:31:24 -0400

    Added documentation for the command line program.
    Added documentation for the command line program. :)

--- src-tauri/src/main_cmd.rs
+++ src-tauri/src/main_cmd.rs
@@ -3,6 +3,9 @@ mod functionality;
 mod directory;
 mod os_calls;
 
+/**
+ * This is the entry point for the application purely for the command line interface.
+ */
 fn main() {
     main_loop::main_loop();
 }
