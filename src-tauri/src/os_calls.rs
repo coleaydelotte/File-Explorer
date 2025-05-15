e19a4712d4aac208a1a238618e6a76cae54bdfef
Cole Aydelottte <cole.aydelotte@gmail.com>
2025-05-15 19:40:23 -0400

    Fleshed out description.
    Fleshed out description. :)

--- src-tauri/src/os_calls.rs
+++ src-tauri/src/os_calls.rs
@@ -3,6 +3,9 @@ use std::process::Command;
 /**
  * This function will open a file in the default program for the file type.
  * It will use the OS-specific command to open the file.
+ * 
+ * Here we use the `Command` struct from the `std::process` module to run the command.
+ * using the `output` method to execute the command and wait for it to finish.
  */
 pub fn open_file(file_path: &str) {
     #[cfg(target_os = "windows")]
