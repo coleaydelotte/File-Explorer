cd45fa12db7e43e8e208747b0d012f67a0856ce3
Cole Aydelottte <cole.aydelotte@gmail.com>
2025-05-16 19:44:23 -0400

    Fixed dependencies.
    Fixed dependencies. :)

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// Had issues with React DOM and Tauri, so I had to use the createRoot method instead of the render method.
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App />
 );
