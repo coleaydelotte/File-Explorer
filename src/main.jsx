25ec0fe3b5717707fae263ea61b863706d216fd5
Cole Aydelottte <cole.aydelotte@gmail.com>
2025-05-25 23:42:38 -0400

    Updated documentation.
    Updated documentation. :)

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// not using strict mode for now
+// Not using strict mode for now because of issue i ran into.
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App />
 );
